088a65cfc0b0d780aae1ae081cefa4481d39a033
Antti Leppä <antti.leppa@foyt.fi>
2014-09-19 07:58:57 +0300

    ** Added method for listing sessions by accessed, type and closed (can be used to identify timed out sessions)

--- src/main/java/fi/otavanopisto/coops/quickstart/dao/CoOpsSessionDAO.java
+++ src/main/java/fi/otavanopisto/coops/quickstart/dao/CoOpsSessionDAO.java
@@ -61,6 +61,24 @@ public class CoOpsSessionDAO extends GenericDAO<CoOpsSession> {
     
     return entityManager.createQuery(criteria).getResultList();
   }
+  
+  public List<CoOpsSession> listByAccessedBeforeAndTypeAndClosed(Date accessed, CoOpsSessionType type, Boolean closed) {
+    EntityManager entityManager = getEntityManager();
+
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<CoOpsSession> criteria = criteriaBuilder.createQuery(CoOpsSession.class);
+    Root<CoOpsSession> root = criteria.from(CoOpsSession.class);
+    criteria.select(root);
+    criteria.where(
+      criteriaBuilder.and(
+        criteriaBuilder.equal(root.get(CoOpsSession_.closed), closed),
+        criteriaBuilder.equal(root.get(CoOpsSession_.type), type),
+        criteriaBuilder.lessThan(root.get(CoOpsSession_.accessed), accessed)
+      )
+    );
+    
+    return entityManager.createQuery(criteria).getResultList();
+  }
 
   public CoOpsSession updateType(CoOpsSession coOpsSession, CoOpsSessionType type) {
     coOpsSession.setType(type);
